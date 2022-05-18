8a338ea291a7cd81e3ca41293f2d425474f8012a
Liu Liu <i@liuliu.me>
2022-05-18 14:56:07 -0400

    Basic parser that does enums.

--- Sources/Mjt.swift
+++ Sources/Mjt.swift
@@ -11,7 +11,250 @@ public enum MjFontScale: Int32 {
 
 public enum MjCatBit: Int32 {
   case `static` = 1
-  case dynamic = 2
+  case `dynamic` = 2
   case decor = 4
   case all = 7
+}
+
+public enum MjDisableBit: Int32 {
+  case constraint = 1
+  case equality = 2
+  case frictionloss = 4
+  case limit = 8
+  case contact = 16
+  case passive = 32
+  case gravity = 64
+  case clampctrl = 128
+  case warmstart = 256
+  case filterparent = 512
+  case actuation = 1024
+  case refsafe = 2048
+}
+
+public enum MjEnableBit: Int32 {
+  case override = 1
+  case energy = 2
+  case fwdinv = 4
+  case sensornoise = 8
+  case multiccd = 1_073_741_824
+}
+
+public enum MjJoint: Int32 {
+  case free = 0
+  case ball
+  case slide
+  case hingeRotationAngleRadAroundBodyFixedAxis_1
+}
+
+public enum MjGeom: Int32 {
+  case plane = 0
+  case hfield
+  case sphere
+  case capsule
+  case ellipsoid
+  case cylinder
+  case box
+  case mesh
+  case arrow = 100
+  case arrow1
+  case arrow2
+  case line
+  case skin
+  case label
+  case none = 1001
+}
+
+public enum MjCamLight: Int32 {
+  case fixed = 0
+  case track
+  case trackcom
+  case targetbody
+  case targetbodycomPosFixedInBody
+}
+
+public enum MjTexture: Int32 {
+  case _2d = 0
+  case cube
+  case skyboxCubeTextureUsedAsSkybox
+}
+
+public enum MjIntegrator: Int32 {
+  case euler = 0
+  case rk4_4thOrderRungeKutta
+}
+
+public enum MjCollision: Int32 {
+  case all = 0
+  case pair
+  case dynamicTestDynamicPairsOnly
+}
+
+public enum MjCone: Int32 {
+  case pyramidal = 0
+  case ellipticElliptic
+}
+
+public enum MjJacobian: Int32 {
+  case dense = 0
+  case sparse
+  case autoDenseIfNv_60
+}
+
+public enum MjSolver: Int32 {
+  case pgs = 0
+  case cg
+  case newtonNewtonPrimal
+}
+
+public enum MjEq: Int32 {
+  case connect = 0
+  case weld
+  case joint
+  case tendon
+  case distanceFixTheContactDistanceBetweentTwoGeoms
+}
+
+public enum MjWrap: Int32 {
+  case none = 0
+  case joint
+  case pulley
+  case site
+  case sphere
+  case cylinderWrapAroundInfiniteCylinder
+}
+
+public enum MjTrn: Int32 {
+  case joint = 0
+  case jointinparent
+  case slidercrank
+  case tendon
+  case site
+  case undefined = 1000
+}
+
+public enum MjDyn: Int32 {
+  case none = 0
+  case integrator
+  case filter
+  case muscle
+  case userUserDefinedDynamicsType
+}
+
+public enum MjGain: Int32 {
+  case fixed = 0
+  case muscle
+  case userUserDefinedGainType
+}
+
+public enum MjBias: Int32 {
+  case none = 0
+  case affine
+  case muscle
+  case userUserDefinedBiasType
+}
+
+public enum MjObj: Int32 {
+  case unknown = 0
+  case body
+  case xbody
+  case joint
+  case dof
+  case geom
+  case site
+  case camera
+  case light
+  case mesh
+  case skin
+  case hfield
+  case texture
+  case material
+  case pair
+  case exclude
+  case equality
+  case tendon
+  case actuator
+  case sensor
+  case numeric
+  case text
+  case tuple
+  case keyKeyframe
+}
+
+public enum MjConstraint: Int32 {
+  case equality = 0
+  case frictionDof
+  case frictionTendon
+  case limitJoint
+  case limitTendon
+  case contactFrictionless
+  case contactPyramidal
+  case contactEllipticFrictionalContact
+}
+
+public enum MjConstraintState: Int32 {
+  case satisfied = 0
+  case quadratic
+  case linearneg
+  case linearpos
+  case coneSquaredDistanceToConeCostEllipticContact
+}
+
+public enum MjSensor: Int32 {
+  case touch = 0
+  case accelerometer
+  case velocimeter
+  case gyro
+  case force
+  case torque
+  case magnetometer
+  case rangefinder
+  case jointpos
+  case jointvel
+  case tendonpos
+  case tendonvel
+  case actuatorpos
+  case actuatorvel
+  case actuatorfrc
+  case ballquat
+  case ballangvel
+  case jointlimitpos
+  case jointlimitvel
+  case jointlimitfrc
+  case tendonlimitpos
+  case tendonlimitvel
+  case tendonlimitfrc
+  case framepos
+  case framequat
+  case framexaxis
+  case frameyaxis
+  case framezaxis
+  case framelinvel
+  case frameangvel
+  case framelinacc
+  case frameangacc
+  case subtreecom
+  case subtreelinvel
+  case subtreeangmom
+  case userSensorDataProvidedByMjcbSensorCallback
+}
+
+public enum MjStage: Int32 {
+  case none = 0
+  case pos
+  case vel
+  case accAccelerationForceDependentComputations
+}
+
+public enum MjDataType: Int32 {
+  case real = 0
+  case positive
+  case axis
+  case quaternionUnitQuaternion
+}
+
+public enum MjLRMode: Int32 {
+  case none = 0
+  case muscle
+  case muscleuser
+  case allProcessAllActuators
 }
---
+++ Sources/codegen/main.swift
@@ -0,0 +1,64 @@
+import ChangeCases
+import Foundation
+
+struct Enum {
+  var name: String
+  var keyValues: [(key: String, value: String?)]
+}
+
+let filePath = CommandLine.arguments[1]
+let fileContent = try! String(contentsOf: URL(fileURLWithPath: filePath), encoding: .utf8)
+let lines = fileContent.split(whereSeparator: \.isNewline)
+
+var enums = [Enum]()
+var thisEnum: Enum? = nil
+
+for line in lines {
+  if let range = line.range(of: #"\s*typedef\s+enum\s+(\w)+\s+\{"#, options: .regularExpression) {
+    let matched = line[range].split(whereSeparator: \.isWhitespace)
+    thisEnum = Enum(name: String(matched[2]), keyValues: [])
+  } else if let currentEnum = thisEnum {
+    if line.range(of: #"\s*\}"#, options: .regularExpression) != nil {
+      enums.append(currentEnum)
+      thisEnum = nil
+    } else {
+      let trimmed = line.trimmingCharacters(in: .whitespacesAndNewlines)
+      guard !trimmed.hasPrefix("mjN") && !trimmed.hasPrefix("/") else { continue }
+      let kv = trimmed.prefix(while: { $0 != "," }).split(separator: "=")
+      var value: String? = nil
+      if kv.count > 1 {
+        value = kv[1].prefix(while: { $0 != "/" }).trimmingCharacters(in: .whitespaces)
+      }
+      thisEnum?.keyValues.append((key: kv[0].trimmingCharacters(in: .whitespaces), value: value))
+    }
+  }
+}
+
+func enumDecl(_ thisEnum: Enum) -> String {
+  precondition(thisEnum.name.hasPrefix("mjt"))
+  let swiftName =
+    "Mj" + thisEnum.name.suffix(from: thisEnum.name.index(thisEnum.name.startIndex, offsetBy: 3))
+  var code =
+    "public enum \(swiftName.prefix(upTo: swiftName.index(swiftName.endIndex, offsetBy: -1))): Int32 {\n"
+  for (key, value) in thisEnum.keyValues {
+    var swiftKey = key.split(separator: "_", maxSplits: 1)[1].lowercased().camelCase()
+    if !swiftKey.prefix(while: \.isNumber).isEmpty {
+      swiftKey = "_" + swiftKey
+    }
+    if var value = value {
+      if value.contains("<<") {  // Evaluate this, Swift is not happy with anything other than literal.
+        let parts = value.components(separatedBy: "<<")
+        value = String(Int(parts[0])! << Int(parts[1])!)
+      }
+      code += "  case \(swiftKey) = \(value)\n"
+    } else {
+      code += "  case \(swiftKey)\n"
+    }
+  }
+  code += "}\n"
+  return code
+}
+
+for thisEnum in enums {
+  print(enumDecl(thisEnum))
+}
