fbef0c6c7ae88119c30026a7f92b059f6c919b1f
Liu Liu <i@liuliu.me>
2022-05-23 22:54:38 -0400

    Feel comfortable that I extracted enough info to build the api map.

--- Sources/codegen/main.swift
+++ Sources/codegen/main.swift
@@ -80,6 +80,8 @@ for filePath in CommandLine.arguments[2...] {
       if !trimmed.contains(");") {
         // We need to continue.
         thisAPIDefinition = apiDefinition
+      } else {
+        apiDefinitions.append(apiDefinition)
       }
     } else if var currentAPIDefinition = thisAPIDefinition {
       let restParameters = line.prefix(while: { $0 != ")" })
@@ -521,6 +523,45 @@ func structExtension(
   return code
 }
 
+func functionExtension(_ apiDefinition: APIDefinition, nameMapping: [String: [String]] = [:])
+  -> String
+{
+  // First, identify primary owner of the function.
+  // Rule:
+  // 1. Only look at first or last parameter (excluding mjt* or ordinary C types) as the primary owner, if cannot find any, fatal.
+  // 2. Identify parameter corresponding to the function signature, if the function starts with mjv_
+  //    find the parameter has type mjv*.
+  guard apiDefinition.parameters.count > 0 else { return "" }
+  let prefix = apiDefinition.name.prefix(while: { $0 != "_" })
+  var mainInd: Int? = nil
+  var mainType: String? = nil
+  for (i, last) in apiDefinition.parameters.enumerated().reversed() {
+    guard !last.type.hasPrefix("mjt") else { continue }
+    let matched = last.type.split(separator: " ", maxSplits: 1)
+    let nakedType = matched.last!
+    guard nakedType.hasPrefix("mj") else { continue }
+    if nakedType.lowercased().hasPrefix(prefix) {
+      mainType = String(nakedType)
+      mainInd = i
+    }
+    break
+  }
+  if let first = apiDefinition.parameters.first {
+    let matched = first.type.split(separator: " ", maxSplits: 1)
+    let nakedType = matched.last!
+    if nakedType.lowercased().hasPrefix(prefix) {
+      mainType = String(nakedType)
+      mainInd = 0
+    } else if mainType == nil {
+      mainType = String(nakedType)
+      mainInd = 0
+    }
+  }
+  guard let mainType = mainType, let mainInd = mainInd else { fatalError() }
+  // Now we have main type, we can create functions for them.
+  return "mainType: \(mainType): \(apiDefinition)"
+}
+
 var mjtCode = ""
 for thisEnum in enums {
   let swiftName_ =
@@ -683,4 +724,141 @@ for thisStruct in structs {
       to: URL(fileURLWithPath: WorkDir).appendingPathComponent("MjuiDef+Extensions.swift"),
       atomically: false, encoding: .utf8)
   }
+}
+
+let FunctionDeny: Set<String> = Set([
+  "mj_defaultVFS",
+  "mj_addFileVFS",
+  "mj_makeEmptyFileVFS",
+  "mj_findFileVFS",
+  "mj_deleteFileVFS",
+  "mj_deleteVFS",
+  "mj_loadXML",
+  "mj_saveLastXML",
+  "mj_freeLastXML",
+  "mj_printSchema",
+  "mj_defaultLROpt",
+  "mj_defaultSolRefImp",
+  "mj_defaultOption",
+  "mj_defaultVisual",
+  "mj_copyModel",
+  "mj_saveModel",
+  "mj_loadModel",
+  "mj_deleteModel",
+  "mj_sizeModel",
+  "mj_makeData",
+  "mj_copyData",
+  "mj_deleteData",
+  "mjv_defaultCamera",
+  "mjv_defaultPerturb",
+  "mjv_defaultOption",
+  "mjv_defaultFigure",
+  "mjv_defaultScene",
+  "mjv_makeScene",
+  "mjv_freeScene",
+  "mjr_defaultContext",
+  "mjr_makeContext",
+  "mjr_freeContext",
+  "mjui_themeSpacing",
+  "mjui_themeColor",
+  "mju_malloc",
+  "mju_free",
+  "mj_activate",
+  "mj_deactivate",
+  // No need for math.
+  "mju_zero3",
+  "mju_copy3",
+  "mju_scl3",
+  "mju_add3",
+  "mju_sub3",
+  "mju_addTo3",
+  "mju_subFrom3",
+  "mju_addToScl3",
+  "mju_addScl3",
+  "mju_normalize3",
+  "mju_norm3",
+  "mju_dot3",
+  "mju_dist3",
+  "mju_rotVecMat",
+  "mju_rotVecMatT",
+  "mju_cross",
+  "mju_zero4",
+  "mju_unit4",
+  "mju_copy4",
+  "mju_normalize4",
+  "mju_zero",
+  "mju_copy",
+  "mju_sum",
+  "mju_L1",
+  "mju_scl",
+  "mju_add",
+  "mju_sub",
+  "mju_addTo",
+  "mju_subFrom",
+  "mju_addToScl",
+  "mju_addScl",
+  "mju_normalize",
+  "mju_norm",
+  "mju_dot",
+  "mju_mulMatVec",
+  "mju_mulMatTVec",
+  "mju_transpose",
+  "mju_mulMatMat",
+  "mju_mulMatMatT",
+  "mju_mulMatTMat",
+  "mju_sqrMatTD",
+  "mju_transformSpatial",
+  "mju_rotVecQuat",
+  "mju_negQuat",
+  "mju_mulQuat",
+  "mju_mulQuatAxis",
+  "mju_axisAngle2Quat",
+  "mju_quat2Vel",
+  "mju_subQuat",
+  "mju_quat2Mat",
+  "mju_mat2Quat",
+  "mju_derivQuat",
+  "mju_quatIntegrate",
+  "mju_quatZ2Vec",
+  "mju_mulPose",
+  "mju_negPose",
+  "mju_trnVecPose",
+  "mju_cholFactor",
+  "mju_cholSolve",
+  "mju_cholUpdate",
+  "mju_eig3",
+  "mju_muscleGain",
+  "mju_muscleBias",
+  "mju_muscleDynamics",
+  "mju_encodePyramid",
+  "mju_decodePyramid",
+  "mju_springDamper",
+  "mju_min",
+  "mju_max",
+  "mju_sign",
+  "mju_round",
+  "mju_str2Type",
+  "mju_isBad",
+  "mju_isZero",
+  "mju_standardNormal",
+  "mju_f2n",
+  "mju_n2f",
+  "mju_d2n",
+  "mju_n2d",
+  "mju_insertionSort",
+  "mju_insertionSortInt",
+  "mju_Halton",
+  "mju_strncpy",
+  "mju_sigmoid",
+])
+
+for apiDefinition in apiDefinitions {
+  guard !FunctionDeny.contains(apiDefinition.name) else { continue }
+  print(
+    functionExtension(
+      apiDefinition,
+      nameMapping: [
+        "m": ["model"], "d": ["data"], "con": ["contact", "context"], "scn": ["scene"],
+        "key": ["keyframe"],
+      ]))
 }
